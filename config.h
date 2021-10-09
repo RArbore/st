7209cecf82bcbe9d7e9d4efc66c1504fea19685f
Russel Arbore <russel@russelarbore.com>
2021-10-08 23:42:47 -0500

    Fixed background

--- config.h
+++ config.h
@@ -149,7 +149,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 15;
-unsigned int defaultbg = 16;
+unsigned int defaultbg = 0;
 static unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 15;
 
